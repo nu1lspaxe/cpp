d7fc0e320d1077c40c821691160192d5a46f73cf
Nu1LSpaxe <nu1lspaxe.anonym0ux@gmail.com>
2024-02-15 11:57:14 +0800

    Add: BK-tree

---
+++ algorithm/bk-tree/docs.md
@@ -0,0 +1,4 @@
+# BK-Tree 
+
+reference: https://www.geeksforgeeks.org/bk-tree-introduction-implementation/?ref=gcse
+
---
+++ algorithm/bk-tree/main.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+
+// Maximum number of words in dict[]
+#define MAXN 100
+// Define the tolerance value
+#define TOL 2
+// Define maximum length of a word
+#define LEN 10
+
+struct Node
+{
+    // stores the word of the current Node
+    std::string word;
+
+    // links to other Node in the tree
+    int next[2 * LEN];
+
+    // Constructors
+    Node() {}
+    Node(std::string x) : word(x)
+    {
+        // Initializing next[i] = 0
+        for (size_t i{0}; i < 2 * LEN; ++i)
+            next[i] = 0;
+    }
+};
+
+// Store the root Node
+Node ROOT;
+
+// Store every Node of the tree
+Node tree[MAXN];
+
+// Index for current Node of tree
+int ptr;
+
+int min(int x, int y, int z) { return std::min(std::min(x, y), z); }
+
+// Edit Distance
+// Dynamic-Approach: Table, O(m*n)
+int editDistance(std::string str1, std::string str2)
+{
+    int m = str1.length(), n = str2.length();
+    
+    // the results will be stored in table
+    int table[m + 1][n + 1];
+
+    // fill the table with bottom-up manner
+    for (size_t i{0}; i <= m; ++i)
+    {
+        for (size_t j{0}; j <= n; ++j)
+        {
+            if (i == 0) table[i][j] = j;
+            else if (j == 0) table[i][j] = i;
+
+            else if (str1[i-1] == str2[j-1]) table[i][j] = table[i-1][j-1];
+
+            else table[i][j] = 1 + min(table[i][j-1], table[i-1][j], table[i-1][j-1]);  // Insert Remove Replace
+        }
+    }
+    return table[m][n];
+}
+
+// Add current Node to the tree
+void add(Node &root, Node &curr)
+{
+    if (root.word == "")
+    {
+        // If it's the first Node then make it the root Node
+        root = curr;
+        return;
+    }
+
+    // Get its edit distance from the root Node
+    int dist = editDistance(curr.word, root.word);
+
+    if (tree[root.next[dist]].word == "")
+    {
+        // If no Node exists at this dist from root
+        // make it child of root Node
+
+        // Incrementing the pointer for curr Node
+        ++ptr;
+
+        // Adding curr Node to the tree
+        tree[ptr] = curr;
+
+        // Curr Node as child of root Node
+        root.next[dist] = ptr;
+    }
+    else
+    {
+        // Recursively find the parent for curr Node
+        add(tree[root.next[dist]], curr);
+    }
+}
+
+std::vector<std::string> getSimilarWords(Node &root, std::string &str)
+{
+    std::vector<std::string> res;
+
+    if (root.word == "")
+        return res;
+
+    // Calculating edit distance of str from root
+    int dist = editDistance(root.word, str);
+
+    // If dist is less than tolerance value
+    // add it to similar words
+    if (dist <= TOL)
+        res.push_back(root.word);
+
+    // Iterate over the string having tolerance in range[D-TOL, D+TOL]
+    int start = dist - TOL;
+    if (start < 0)
+        start = 1;
+
+    while (start <= dist + TOL)
+    {
+        std::vector<std::string> tmp = getSimilarWords(tree[root.next[start]], str);
+
+        for (auto i : tmp)
+            res.push_back(i);
+
+        ++start;
+    }
+
+    return res;
+}
+
+int main()
+{
+    std::string dictionary[] = {
+        "hell", "help", "shell", "smell", "fell",
+        "felt", "oops", "pop", "oouch", "halt"};
+
+    ptr = 0;
+    int length = std::size(dictionary);
+
+    // Add dictionary words to tree
+    for (size_t i{0}; i < length; ++i)
+    {
+        Node node = Node(dictionary[i]);
+        add(ROOT, node);
+    }
+
+    std::vector<std::string> match;
+
+    std::string str1 = "ops";
+    match = getSimilarWords(ROOT, str1);
+    std::cout << "Similar words in dictionary for " << str1 << ": ";
+    for (auto x: match)
+        std::cout << x << " ";
+    std::cout << std::endl;
+
+    std::string str2 = "helt";
+    match = getSimilarWords(ROOT, str2);
+    std::cout << "Similar words in dictionary for " << str2 << ": ";
+    for (auto x: match)
+        std::cout << x << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
